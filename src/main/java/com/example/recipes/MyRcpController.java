044df6402746c7af06f028bc4d36cbe82c0ac7b2
CD-PC <cdavid0504@gmail.com>
2025-11-29 09:33:32 -0500

    Updated RCP codes

--- src/main/java/com/example/recipes/MyRcpController.java
+++ src/main/java/com/example/recipes/MyRcpController.java
@@ -1,52 +1,80 @@
 package com.example.recipes;
 
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class MyRcpController implements Initializable {
 
-    @FXML
-    private Label myLabel;
+    @FXML private Label topTitle;
+    @FXML private ComboBox<String> cuisineBox;
+    @FXML private ComboBox<String> proteinBox;
+    @FXML private ComboBox<String> timeBox;
+    @FXML private ComboBox<String> difficultyBox;
 
-    @FXML
-    private ChoiceBox<String> myCuisineChoicebox;
+    @FXML private ListView<String> recipeList;
+    @FXML private TextArea recipeDetails;
 
-    @FXML
-    private ChoiceBox<String> myProteinChoicebox;
 
-    @FXML
-    private ChoiceBox<String> myLengthChoicebox;
-
-    @FXML
-    private ChoiceBox<String> myDiffChoicebox;
-
-    @FXML
-    private TextField myTextField;
-
-    private String[] cuisine = {"Chinese" , "English" , "Italian" , "Spanish" , "Middle Eastern" , "Japanese"};
-    private String[] protein = {"Chicken" , "Beef" , "Bean" , "Tofu" , "Fish" , "Shellfish" , "Veal" , "Lamb"};
-    private String[] time = {"30 minutes No prep" , "30 minutes Prep" , "60-120 minutes No prep", "60-120 minutes Prep", "Multiple Hours prep"};
-    private String[] diff = {"Easy", "Medium", "Hard", "Chef Level"};
+    private final String[] cuisine = { "Italian", "Indian", "Chinese", "American" };
+    private final String[] protein = { "Chicken", "Beef", "Vegetarian", "Fish" };
+    private final String[] time = { "Under 20 min", "30-45 min", "1 hour+" };
+    private final String[] diff = { "Easy", "Medium", "Hard" };
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        myCuisineChoicebox.getItems().addAll(cuisine);
-        myProteinChoicebox.getItems().addAll(protein);
-        myLengthChoicebox.getItems().addAll(time);
-        myDiffChoicebox.getItems().addAll(diff);
+    public void initialize(URL url, ResourceBundle rb) {
+        // populate the search
+        cuisineBox.getItems().addAll(cuisine);
+        proteinBox.getItems().addAll(protein);
+        timeBox.getItems().addAll(time);
+        difficultyBox.getItems().addAll(diff);
 
+        // sample recipe list
+        recipeList.getItems().addAll(
+                "Chicken Marsala",
+                "Chicken Parmesan",
+                "Chicken Tikka Masala",
+                "Veal Cutlet",
+                "Vegetarian Shashuka"
+        );
+
+        // when a recipe is selected, load details into the right pane
+        recipeList.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                loadRecipe(newVal);
+            }
+        });
+    }
+
+
+    private void loadRecipe(String recipeName) {
+        String sample = recipeName + "\n\nIngredients:\n- Ingredient A\n- Ingredient B\n\nSteps:\n1) Step one...\n2) Step two...\n3) Serve.";
+        recipeDetails.setText(sample);
+    }
+
+
+    @FXML
+    private void showFavorites(ActionEvent e) {
+        // implement your favorites filtering / logic
+        recipeList.getItems().clear();
+        recipeList.getItems().addAll(
+                "Chicken Marsala",
+                "Chicken Parmesan",
+                "Vegetarian Shashuka"
+        );
+    }
+
+    @FXML
+    private void goHome(ActionEvent e) throws IOException {
+        new SceneController().switchToMyRcp(e);
     }
 }
--- src/main/java/com/example/recipes/ShareRcpController.java
+++ src/main/java/com/example/recipes/ShareRcpController.java
@@ -1,4 +1,50 @@
 package com.example.recipes;
 
-public class ShareRcpController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShareRcpController implements Initializable {
+
+    @FXML private ChoiceBox<String> cuisineChoice;
+    @FXML private ChoiceBox<String> proteinChoice;
+    @FXML private ChoiceBox<String> timeChoice;
+    @FXML private ChoiceBox<String> difficultyChoice;
+
+    @FXML private TextArea recipeText;
+    @FXML private ImageView imagePreview;
+
+    private final String[] cuisine = {"Chinese", "English", "Italian", "Spanish", "Middle Eastern", "Japanese"};
+    private final String[] protein = {"Chicken", "Beef", "Bean", "Tofu", "Fish", "Shellfish", "Veal", "Lamb"};
+    private final String[] time = {"30 minutes No prep", "30 minutes Prep", "60-120 minutes No prep", "60-120 minutes Prep", "Multiple Hours prep"};
+    private final String[] diff = {"Easy", "Medium", "Hard", "Chef Level"};
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (cuisineChoice != null) cuisineChoice.getItems().addAll(cuisine);
+        if (proteinChoice != null) proteinChoice.getItems().addAll(protein);
+        if (timeChoice != null) timeChoice.getItems().addAll(time);
+        if (difficultyChoice != null) difficultyChoice.getItems().addAll(diff);
+    }
+
+    @FXML
+    private void uploadImage(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Select Recipe Image");
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+        File file = chooser.showOpenDialog(null);
+        if (file != null) {
+            imagePreview.setImage(new Image(file.toURI().toString()));
+        }
+    }
+
 }
